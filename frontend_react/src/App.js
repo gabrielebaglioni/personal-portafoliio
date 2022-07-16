a63ad3b068d2f5a5b3fcd7fb60b2e8c1262e4d07
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-07-16 19:33:24 +0200

    add general scaffolding

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,18 @@
 
-import './App.css';
+import React from 'react';
+import { About, Footer, Header, Skills, Testimonial, Work} from './container';
+import { Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/> 
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/> 
     </div>
   );
 }
